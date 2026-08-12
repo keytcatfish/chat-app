9ff2aecb7a1d825512f16669ed867b5221c659f8
keycatfish <abdulkadirkat666@gmail.com>
2026-08-12 12:43:16 +0300

    sql.js ile kalici mesaj gecmisi

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-const sqlite3 = require('sqlite3').verbose();
+const initSqlJs = require('sql.js');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,62 +10,78 @@ const io = socketio(server);
 
 app.use(express.static('public'));
 
-const db = new sqlite3.Database('chat.db');
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS mesajlar (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    kullanici TEXT,
-    mesaj TEXT,
-    zaman INTEGER
-  )
-`);
-
 const onlineKullanicilar = new Set();
+let db;
+const DB_PATH = './chat.db';
 
-io.on('connection', (socket) => {
+initSqlJs().then((SQL) => {
+  if (fs.existsSync(DB_PATH)) {
+    const fileBuffer = fs.readFileSync(DB_PATH);
+    db = new SQL.Database(fileBuffer);
+  } else {
+    db = new SQL.Database();
+  }
 
-  socket.on('katil', (kullanici) => {
-    socket.kullanici = kullanici;
-    onlineKullanicilar.add(kullanici);
-    io.emit('online', Array.from(onlineKullanicilar));
-    db.all('SELECT * FROM mesajlar ORDER BY zaman ASC', (err, rows) => {
-      socket.emit('gecmis', rows);
-    });
-    io.emit('sistem', kullanici + ' odaya katıldı');
-  });
+  db.run(`
+    CREATE TABLE IF NOT EXISTS mesajlar (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      kullanici TEXT,
+      mesaj TEXT,
+      zaman INTEGER
+    )
+  `);
 
-  socket.on('mesaj', (data) => {
-    db.run('INSERT INTO mesajlar (kullanici, mesaj, zaman) VALUES (?, ?, ?)',
-      [data.kullanici, data.mesaj, Date.now()]);
-    io.emit('mesaj', data);
-  });
+  function kaydet() {
+    const data = db.export();
+    fs.writeFileSync(DB_PATH, Buffer.from(data));
+  }
 
-  socket.on('disconnect', () => {
-    if (socket.kullanici) {
-      onlineKullanicilar.delete(socket.kullanici);
+  io.on('connection', (socket) => {
+
+    socket.on('katil', (kullanici) => {
+      socket.kullanici = kullanici;
+      onlineKullanicilar.add(kullanici);
       io.emit('online', Array.from(onlineKullanicilar));
-      io.emit('sistem', socket.kullanici + ' ayrıldı');
-    }
+      const rows = db.exec('SELECT kullanici, mesaj FROM mesajlar ORDER BY zaman ASC');
+      const mesajlar = rows.length > 0 ? rows[0].values.map(r => ({ kullanici: r[0], mesaj: r[1] })) : [];
+      socket.emit('gecmis', mesajlar);
+      io.emit('sistem', kullanici + ' odaya katıldı');
+    });
+
+    socket.on('mesaj', (data) => {
+      db.run('INSERT INTO mesajlar (kullanici, mesaj, zaman) VALUES (?, ?, ?)',
+        [data.kullanici, data.mesaj, Date.now()]);
+      kaydet();
+      io.emit('mesaj', data);
+    });
+
+    socket.on('disconnect', () => {
+      if (socket.kullanici) {
+        onlineKullanicilar.delete(socket.kullanici);
+        io.emit('online', Array.from(onlineKullanicilar));
+        io.emit('sistem', socket.kullanici + ' ayrıldı');
+      }
+    });
+
   });
 
-});
+  function geceyarisisiSifirla() {
+    const simdi = new Date();
+    const geceyarisi = new Date();
+    geceyarisi.setHours(24, 0, 0, 0);
+    const kalan = geceyarisi - simdi;
+    setTimeout(() => {
+      db.run('DELETE FROM mesajlar');
+      kaydet();
+      io.emit('sistem', 'Mesajlar sıfırlandı. Yeni gün başladı.');
+      io.emit('gecmis', []);
+      geceyarisisiSifirla();
+    }, kalan);
+  }
 
-function geceyarisisiSifirla() {
-  const simdi = new Date();
-  const geceyarisi = new Date();
-  geceyarisi.setHours(24, 0, 0, 0);
-  const kalan = geceyarisi - simdi;
-  setTimeout(() => {
-    db.run('DELETE FROM mesajlar');
-    io.emit('sistem', 'Mesajlar sıfırlandı. Yeni gün başladı.');
-    io.emit('gecmis', []);
-    geceyarisisiSifirla();
-  }, kalan);
-}
+  geceyarisisiSifirla();
 
-geceyarisisiSifirla();
-
-server.listen(3000, () => {
-  console.log('Sunucu 3000 portunda çalışıyor');
+  server.listen(3000, () => {
+    console.log('Sunucu 3000 portunda çalışıyor');
+  });
 });
